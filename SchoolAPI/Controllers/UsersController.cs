3aec4a99900c4732064b260400f5e06b98f7d3f9
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-06-14 12:01:32 -0600

    Jwt Authorization

---
+++ SchoolAPI/Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPI.Models;
+using SchoolAPI.Models.Dto;
+using SchoolAPI.Repository.IRepository;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SchoolAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+        private readonly IConfiguration _config;
+
+        public UsersController(IUserRepository userRepo, IConfiguration config)
+        {
+            _userRepo = userRepo;
+            _config = config;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UserCreateDto userDto)
+        {
+            if (userDto == null)
+                return BadRequest();
+
+            string name = userDto.UserName.ToString();
+            string pass = userDto.Password.ToString();
+
+            var user = await _userRepo.GetUser(name, pass);
+
+            if (user == null)
+                return NotFound();
+
+            var jwt = _config.GetSection("Jwt").Get<Jwt>();
+
+            // Crear los claims
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Crear el token
+            var token = new JwtSecurityToken(
+                jwt.Issuer,
+                jwt.Audience,
+                claims,
+                expires: DateTime.Now.AddMinutes(4),
+                signingCredentials: credentials
+                );
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
--- SchoolAPI/Models/User.cs
+++ SchoolAPI/Models/User.cs
@@ -4,8 +4,9 @@ namespace SchoolAPI.Models
 {
     public class User
     {
-        [Key]
+        public int Id { get; set; }
         public string? UserName { get; set; }
         public string? Password { get; set; }
+        public string? Role { get; set; }
     }
 }
